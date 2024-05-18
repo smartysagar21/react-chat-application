dcfa120810232fdeee3c092d7d6e364ac3f8fa4e
smartysagar21 <smartysagar21@gmail.com>
2024-05-18 17:18:38 +0530

    Add initial frontend code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import List from "./components/list/List";
+import Detail from "./components/detail/Detail";
+import Chat from "./components/chat/Chat";
+
 const App = () => {
   return (
-    <div className=''>App</div>
-  )
-}
+    <div className="container">
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
+};
 
-export default App
+export default App;
