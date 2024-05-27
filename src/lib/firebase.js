6d0ce594d24364eb48f8b021aa6b68306769aa58
smartysagar21 <smartysagar21@gmail.com>
2024-05-27 17:13:25 +0530

    Configure firbase for authentication db and storage

---
+++ src/lib/firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "reactchatapp-4379c.firebaseapp.com",
+  projectId: "reactchatapp-4379c",
+  storageBucket: "reactchatapp-4379c.appspot.com",
+  messagingSenderId: "219731725427",
+  appId: "1:219731725427:web:5d95d5974c4a5846efae06",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+
+export const db = getFirestore();
+
+export const storage = getStorage();
